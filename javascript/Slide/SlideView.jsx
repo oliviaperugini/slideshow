f6b36b1fe9ac00e148c9d7db96951974137d4b12
Olivia Perugini <oliviaperugini@gmail.com>
2018-07-06 16:55:52 -0400

    Adding SlideView skeleton.

---
+++ javascript/Slide/SlideView.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import SlideTitle from './SlideTitle.jsx'
+import SlideTextBox from './SlideTextBox.jsx'
+import {Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption} from 'reactstrap'
+
+export default class SlideView extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    
+  }
+}
